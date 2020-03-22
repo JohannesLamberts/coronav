b229d248d88d7f21225d77a6748104e7f73f3ecb
Johannes Lamberts <mail@j-lamberts.de>
2020-03-22 14:07:26 +0100

    feat: add hotline search and risk areas (#1)
    * Add hotline-search.vue; Add hotline search localization
    
    * Add risk-areas.vue and localization
    
    * Delet risk-areas.json; Move label into hotline search input field
    
    * Change attribute order
    
    Co-authored-by: Stefanie Stoppel <sstoppel@inovex.de>

--- assets/locales/de.js
+++ assets/locales/de.js
@@ -13,5 +13,39 @@ export default {
     noTestNoSymptomsRisk:
       'Sie gehören zur Risikogruppe -> Zu Hause, auf Symptome achten.',
     noTestNoSymptomsNoRisk: 'Alles ist gut. Kontakte vermeiden.'
+  },
+  components: {
+    hotlineSearch: {
+      labelText: 'Postleitzahl eingeben',
+      buttonText: 'Hotline suchen'
+    },
+    riskAreas: {
+      title: 'Risikogebiete',
+      countries: [
+        { name: 'Ägypten', riskArea: 'ganzes Land' },
+        { name: 'China', riskArea: 'Provinz Hubei (inkl. Stadt Wuhan)' },
+        {
+          name: 'Frankreich',
+          riskArea:
+            'Region Grand Est (diese Region enthält Elsass, Lothringen und Champagne-Ardenne)'
+        },
+        { name: 'Iran', riskArea: 'ganzes Land' },
+        { name: 'Italien', riskArea: 'ganzes Land' },
+        { name: 'Österreich', riskArea: 'Bundesland Tirol' },
+        { name: 'Spanien', riskArea: 'Madrid' },
+        {
+          name: 'Südkorea',
+          riskArea: 'Provinz Gyeongsangbuk-do (Nord-Gyeongsang)'
+        },
+        {
+          name: 'USA',
+          riskArea: 'Bundesstaaten Kalifornien, Washington und New York'
+        }
+      ],
+      germany: {
+        title: 'Besonders betroffene Gebiete in Deutschland',
+        areas: [{ name: 'Landkreis Heinsberg (Nordrhein-Westfalen)' }]
+      }
+    }
   }
 }
--- components/hotline-search.vue
+++ components/hotline-search.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    Hotline Search
-    <b-input v-model="plz" />
-    <b-button variant="link">GO</b-button>
-  </div>
+  <b-form-group>
+    <b-form-input
+      v-model="zipCode"
+      :placeholder="$t('components.hotlineSearch.labelText')"
+      type="text"
+      @keypress.enter="goToRkiPLZTool"
+    />
+    <b-button type="button" @click="goToRkiPLZTool">{{
+      $t('components.hotlineSearch.buttonText')
+    }}</b-button>
+  </b-form-group>
 </template>
 
 <script>
+import { rkiPLZTool } from 'static/uris.json'
+
 export default {
   name: 'HotlineSearch',
   data() {
     return {
-      plz: ''
+      labelText: 'Postleitzahl eingeben',
+      buttonText: 'Hotline suchen',
+      zipCode: null
+    }
+  },
+  methods: {
+    goToRkiPLZTool() {
+      const url = rkiPLZTool + this.zipCode
+      window.open(url, '_blank', 'noopener')
     }
   }
 }
