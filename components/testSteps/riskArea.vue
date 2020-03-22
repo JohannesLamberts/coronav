0b8ae4700ff48f1dafd1210dac1571845694d376
Johannes Lamberts <mail@j-lamberts.de>
2020-03-22 19:38:33 +0100

    feat: reformat risk areas

--- components/testSteps/riskArea.vue
+++ components/testSteps/riskArea.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <h2>{{ $t('components.riskAreas.title') }}</h2>
-    <ul>
-      <li v-for="(country, index) of riskAreasCountries" :key="index">
-        <strong>{{ country.name }}:</strong> {{ country.riskArea }}
-      </li>
-    </ul>
-    <h2>{{ $t('components.riskAreas.germany.title') }}</h2>
-    <ul>
-      <li v-for="(area, index) of riskAreasGermany" :key="index">
-        {{ area.name }}
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li>
+      <strong>{{ $t('components.riskAreas.title') }}</strong>
+      <ul>
+        <li v-for="(country, index) of riskAreasCountries" :key="index">
+          <strong>{{ country.name }}:</strong> {{ country.riskArea }}
+        </li>
+      </ul>
+    </li>
+    <li>
+      <strong>{{ $t('components.riskAreas.germany.title') }}</strong>
+      <ul>
+        <li v-for="(area, index) of riskAreasGermany" :key="index">
+          {{ area.name }}
+        </li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <script>
