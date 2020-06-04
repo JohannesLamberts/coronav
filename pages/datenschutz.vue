a260970215135f85de08706ca6a06a4b40d6e7b9
Johannes Lamberts <mail@j-lamberts.de>
2020-06-04 12:51:25 +0200

    feat: add privacy page

---
+++ pages/datenschutz.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div>
+      <h1>
+        {{ $t('privacy.title') }}
+      </h1>
+      <h2 class="h6">Verantwortlicher</h2>
+      <p>
+        Für die Datenverarbeitung auf dieser Website ist der im Impressum
+        genannte Betreiber verantwortlich.
+      </p>
+      <h2 class="h6">Welche Daten werden von uns erhoben?</h2>
+      <p>
+        Wir legen großen Wert auf Datenschutz. Daher haben wir im Rahmen der
+        Website die Verwendung von Tracking / Analytics und Cookies verzichtet
+        und sammeln selbst keinerlei personenbezogenen Daten.
+      </p>
+      <h2 class="h6">Hosting</h2>
+      <p>
+        Diese Website wird bei einem externen Dienstleister gehostet (Hoster).
+        Der Hoster speichert zu jeder Anforderungen durch den Browser einige
+        Informationen. Die Informationen sind ggf. abhängig von ihrem Browser
+        und von Ihnen gewählten Einstellungen. In der Regel werden folgende
+        Informationen gespeichert:
+      </p>
+      <ul>
+        <li>Datum und Uhrzeit</li>
+        <li>Art des Aufrufs</li>
+        <li>Browsertyp und -version</li>
+        <li>IP-Adresse</li>
+        <li>Aufgerufene Datei</li>
+        <li>Status</li>
+      </ul>
+      <p>
+        Unsere Website wird von
+        <a
+          href="https://www.netlify.com/"
+          target="_blank"
+          rel="noreferrer noreferrer"
+          >Netlify</a
+        >
+        gehosted. Die
+        <a
+          href="https://www.netlify.com/privacy/"
+          target="_blank"
+          rel="noreferrer noreferrer"
+          >Datenschutzerklärung</a
+        >
+        und
+        <a
+          href="https://www.netlify.com/gdpr/"
+          target="_blank"
+          rel="noreferrer noreferrer"
+          >Informationen zur DSGVO</a
+        >
+        von Netlify können online eingesehen werden.
+      </p>
+      <p>
+        Nach Angaben von Netlify werden alle geloggten Informationen für weniger
+        als 30 Tage gespeichert.
+      </p>
+    </div>
+  </div>
+</template>
